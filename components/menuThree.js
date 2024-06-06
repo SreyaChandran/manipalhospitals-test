bdd0d3a9bd11f41c5b4f47e8f30d2246935def45
Sreya Chandran <sreya.c@appinessworld.com>
2024-06-06 15:42:53 +0530

    merge code

--- components/menuThree.js
+++ components/menuThree.js
@@ -21,8 +21,13 @@ const MenuThree = () => {
   return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button variant="transperant" aria-label='menu'>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 24 24" fill="none" stroke="#284797" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu">
+                <Button variant="transperant" aria-label='menu' className='p-0'>
+                    <svg className='hidden md:inline' xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu">
+                    <line x1="10" x2="20" y1="12" y2="12"/>
+                    <line x1="4" x2="20" y1="6" y2="6"/>
+                    <line x1="4" x2="20" y1="18" y2="18"/>
+                    </svg>
+                    <svg className="inline md:hidden" xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 24 24" fill="none" stroke="#284797" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu">
                     <line x1="10" x2="20" y1="12" y2="12"/>
                     <line x1="4" x2="20" y1="6" y2="6"/>
                     <line x1="4" x2="20" y1="18" y2="18"/>
@@ -37,35 +42,38 @@ const MenuThree = () => {
                 </svg>
             </DropdownMenuTrigger> */}
             <DropdownMenuContent className="w-[230px]">
-        {/* <DropdownMenuSeparator /> */}
                 <DropdownMenuGroup className='min-[768px]:hidden'>
-                    <DropdownMenuItem className='rounded-none border-b-[1px] border-b-slate-400/60 text-[13px] font-medium text-slate-500 hover:text-primary'>Centre of Excellence</DropdownMenuItem>
-                    <DropdownMenuItem className='rounded-none border-b-[1px] border-b-slate-400/60 text-[13px] font-medium text-slate-500 hover:text-primary'>Doctors</DropdownMenuItem>
-                    <DropdownMenuItem className='rounded-none border-b-[1px] border-b-slate-400/60 text-[13px] font-medium text-slate-500 hover:text-primary'>International Patients</DropdownMenuItem>
+                    <DropdownMenuItem className='icon-menu-item rounded-none font-medium text-slate-500 text-[13px] '>Centre of Excellence</DropdownMenuItem>
+                    <DropdownMenuItem className='icon-menu-item rounded-none font-medium text-slate-500 text-[13px] '>Doctors</DropdownMenuItem>
+                    <DropdownMenuItem className='icon-menu-item rounded-none font-medium text-slate-500 text-[13px] '>International Patients</DropdownMenuItem>
                 </DropdownMenuGroup>
                 <DropdownMenuGroup>
-                    <DropdownMenuItem className='rounded-none border-b-[1px] border-b-slate-400/60 text-[13px] font-medium text-slate-500 hover:text-primary'>Self Registration</DropdownMenuItem>
-                    <DropdownMenuItem className='rounded-none border-b-[1px] border-b-slate-400/60 text-[13px] font-medium text-slate-500 hover:text-primary'>In-Patient Deposit</DropdownMenuItem>
-                    <DropdownMenuItem className='rounded-none border-b-[1px] border-b-slate-400/60 text-[13px] font-medium text-slate-500 hover:text-primary'>Mars - Ambulance</DropdownMenuItem>
-                    <DropdownMenuItem className='rounded-none border-b-[1px] border-b-slate-400/60 text-[13px] font-medium text-slate-500 hover:text-primary'>Home Care</DropdownMenuItem>
-                    <DropdownMenuItem className='rounded-none border-b-[1px] border-b-slate-400/60 text-[13px] font-medium text-slate-500 hover:text-primary'>Organ Donation</DropdownMenuItem>
-                    <DropdownMenuItem className='rounded-none border-b-[1px] border-b-slate-400/60 text-[13px] font-medium text-slate-500 hover:text-primary'>Corporate & PSU</DropdownMenuItem>
-                    <DropdownMenuItem className='rounded-none border-b-[1px] border-b-slate-400/60 text-[13px] font-medium text-slate-500 hover:text-primary'>TPA & Insurance</DropdownMenuItem>
-                    <DropdownMenuItem className='rounded-none border-b-[1px] border-b-slate-400/60 text-[13px] font-medium text-slate-500 hover:text-primary'>Awards And Achievements</DropdownMenuItem>
+                    <DropdownMenuItem className='icon-menu-item rounded-none font-medium text-slate-500 text-[13px] '>Self Registration</DropdownMenuItem>
+                    <DropdownMenuItem className='icon-menu-item rounded-none font-medium text-slate-500 text-[13px] '>In-Patient Deposit</DropdownMenuItem>
+                    <DropdownMenuItem className='icon-menu-item rounded-none font-medium text-slate-500 text-[13px] '>Mars - Ambulance</DropdownMenuItem>
+                    <DropdownMenuItem className='icon-menu-item rounded-none font-medium text-slate-500 text-[13px] '>Home Care</DropdownMenuItem>
+                    <DropdownMenuItem className='icon-menu-item rounded-none font-medium text-slate-500 text-[13px] '>Organ Donation</DropdownMenuItem>
                 </DropdownMenuGroup>
-                <DropdownMenuGroup className='flex flex-row item-center justify-center gap-2 pt-2 pb-[1px]'>
-                    <DropdownMenuItem className='rounded-none border-r-[1px] border-r-slate-400/60 text-[12px] font-medium text-slate-500 hover:text-primary'>About Us</DropdownMenuItem>
-                    <DropdownMenuItem className='rounded-none border-r-[1px] border-r-slate-400/60 text-[12px] font-medium text-slate-500 hover:text-primary'>Events</DropdownMenuItem>
-                    <DropdownMenuItem className='rounded-none text-[12px] font-medium text-slate-500 hover:text-primary '>Media</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                    <DropdownMenuItem className='icon-menu-item rounded-none font-medium text-slate-500 text-[13px] '>Corporate & PSU</DropdownMenuItem>
+                    <DropdownMenuItem className='icon-menu-item rounded-none font-medium text-slate-500 text-[13px] '>TPA & Insurance</DropdownMenuItem>
+                    <DropdownMenuItem className='icon-menu-item rounded-none font-medium text-slate-500 text-[13px] '>Awards And Achievements</DropdownMenuItem>
                 </DropdownMenuGroup>
-                <DropdownMenuGroup className='flex flex-row items-center justify-center gap-2 pb-2'>
-                    <DropdownMenuItem className='rounded-none border-r-[1px] border-r-slate-400/60 text-[12px] font-medium text-slate-500 hover:text-primary '>Blog</DropdownMenuItem>
-                    <DropdownMenuItem className='rounded-none border-r-[1px] border-r-slate-400/60 text-[12px] font-medium text-slate-500 hover:text-primary '>Mobile App</DropdownMenuItem>
-                    <DropdownMenuItem className='rounded-none text-[12px] font-medium text-slate-500 hover:text-primary '>Gallery</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup className='flex flex-row item-center justify-center gap-2 pt-2 pb-[1px] icon-menu-item '>
+                    <DropdownMenuItem className='rounded-none border-r-[1px] border-r-slate-400/60  font-medium text-slate-500 text-[13px]'>About Us</DropdownMenuItem>
+                    <DropdownMenuItem className='rounded-none border-r-[1px] border-r-slate-400/60  font-medium text-slate-500 text-[13px]'>Events</DropdownMenuItem>
+                    <DropdownMenuItem className='rounded-none  font-medium text-slate-500 text-[13px]'>Media</DropdownMenuItem>
                 </DropdownMenuGroup>
-                <DropdownMenuItem className='rounded-none border-b-[1px] border-b-slate-400/60 text-[13px] font-medium text-slate-500 hover:text-primary'>Careers</DropdownMenuItem>
+                <DropdownMenuGroup className='flex flex-row items-center justify-center gap-2 pb-2 icon-menu-item '>
+                    <DropdownMenuItem className='rounded-none border-r-[1px] border-r-slate-400/60  font-medium text-slate-500 text-[13px]'>Blog</DropdownMenuItem>
+                    <DropdownMenuItem className='rounded-none border-r-[1px] border-r-slate-400/60  font-medium text-slate-500 text-[13px]'>Mobile App</DropdownMenuItem>
+                    <DropdownMenuItem className='rounded-none  font-medium text-slate-500 text-[13px]'>Gallery</DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuItem className='icon-menu-item rounded-none font-medium text-slate-500 text-[13px] '>Careers</DropdownMenuItem>
                 <DropdownMenuItem>
-                    <Button variant="transparant" className='text-[13px] font-medium text-slate-500 hover:text-primary border-[1px] border-slate-400/60 hover:border-primary'>Contact Us</Button>
+                    <Button variant="transparant" className='font-medium text-slate-500 text-[13px] hover:text-primary border-[1px] border-slate-400/60 hover:border-primary'>Contact Us</Button>
                 </DropdownMenuItem>
         <DropdownMenuSeparator />
       </DropdownMenuContent>
