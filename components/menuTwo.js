8146142604479456c8cabeecab623c93b97492ec
Sreya Chandran <sreya.c@appinessworld.com>
2024-06-06 14:51:46 +0530

    merge code

--- components/menuTwo.js
+++ components/menuTwo.js
@@ -17,7 +17,7 @@ const MenuTwo = () => {
     <NavigationMenu>
         <NavigationMenuList>
             <NavigationMenuItem>
-                <NavigationMenuTrigger className='h-[28px] text-primary text-[13px] font-primary flex font-semibold flex-row gap-[2px] px-[4px] rounded-full' aria-label='Language'>
+                <NavigationMenuTrigger className='h-[35px] text-primary text-[13px] font-primary flex font-semibold flex-row gap-[2px] px-[4px] rounded-full' aria-label='Language'>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#284797" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-globe"><circle cx="12" cy="12" r="10"/><path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"/><path d="M2 12h20"/></svg>
                     <span className='max-[575px]:hidden'>EN</span>
                 </NavigationMenuTrigger>
@@ -27,7 +27,7 @@ const MenuTwo = () => {
             </NavigationMenuItem>
             <NavigationMenuItem>
                 <Link href="#" legacyBehavior passHref aria-label='podcast'>
-                    <NavigationMenuLink aria-label='podcast' className='h-[28px] bg-white text-primary text-[13px] font-semibold font-primary flex flex-row gap-[2px] px-[2px] md:px-[8px] rounded-full items-center'>
+                    <NavigationMenuLink aria-label='podcast' className='h-[35px] bg-white text-primary text-[13px] font-semibold font-primary flex flex-row gap-[2px] px-[8px] rounded-full items-center'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#284797" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mic"><path d="M12 2a3 3 0 0 0-3 3v7a3 3 0 0 0 6 0V5a3 3 0 0 0-3-3Z"/><path d="M19 10v2a7 7 0 0 1-14 0v-2"/><line x1="12" x2="12" y1="19" y2="22"/></svg>
                         <span className='max-[992px]:hidden'>Podcast</span>
                     </NavigationMenuLink>
@@ -36,7 +36,7 @@ const MenuTwo = () => {
             <NavigationMenuItem className='max-[768px]:hidden'>
                 <Button variant="transperant" className='p-0' aria-label='Search'>
                     <Link href="#" legacyBehavior passHref>
-                    <NavigationMenuLink className='h-[28px] bg-white text-primary text-[13px] font-semibold font-primary flex flex-row gap-[2px] px-[8px] rounded-full items-center'>
+                    <NavigationMenuLink className='h-[35px] bg-white text-primary text-[13px] font-semibold font-primary flex flex-row gap-[2px] px-[8px] rounded-full items-center'>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#284797" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>
                         <span  className='max-[992px]:hidden'>Search</span>
                     </NavigationMenuLink>
