42c15599ca932333dd390571cc58793a712dc52b
Sreya Chandran <sreya.c@appinessworld.com>
2024-06-06 14:07:28 +0530

    merge code

--- components/ui/navigation-menu.tsx
+++ components/ui/navigation-menu.tsx
@@ -30,7 +30,7 @@ const NavigationMenuList = React.forwardRef<
   <NavigationMenuPrimitive.List
     ref={ref}
     className={cn(
-      "group flex flex-1 list-none items-center justify-center space-x-0 gap-2",
+      "group flex flex-1 list-none items-center justify-center space-x-0 gap-1 md:gap-2",
       className
     )}
     {...props}
