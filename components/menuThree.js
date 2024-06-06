f797313ac6cd8bcc147e72e799a3b91326b154ea
Sreya Chandran <sreya.c@appinessworld.com>
2024-06-06 13:51:57 +0530

    merge code

--- components/menuThree.js
+++ components/menuThree.js
@@ -21,7 +21,7 @@ const MenuThree = () => {
   return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button variant="transperant">
+                <Button variant="transperant" aria-label='menu'>
                     <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 24 24" fill="none" stroke="#284797" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu">
                     <line x1="10" x2="20" y1="12" y2="12"/>
                     <line x1="4" x2="20" y1="6" y2="6"/>
--- components/menuTwo.js
+++ components/menuTwo.js
@@ -27,7 +27,7 @@ const MenuTwo = () => {
             </NavigationMenuItem>
             <NavigationMenuItem>
                 <Link href="#" legacyBehavior passHref aria-label='podcast'>
-                    <NavigationMenuLink className='h-[28px] bg-white text-primary text-[13px] font-semibold font-primary flex flex-row gap-[2px] px-[8px] rounded-full items-center'>
+                    <NavigationMenuLink aria-label='podcast' className='h-[28px] bg-white text-primary text-[13px] font-semibold font-primary flex flex-row gap-[2px] px-[8px] rounded-full items-center'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#284797" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mic"><path d="M12 2a3 3 0 0 0-3 3v7a3 3 0 0 0 6 0V5a3 3 0 0 0-3-3Z"/><path d="M19 10v2a7 7 0 0 1-14 0v-2"/><line x1="12" x2="12" y1="19" y2="22"/></svg>
                         <span className='max-[992px]:hidden'>Podcast</span>
                     </NavigationMenuLink>
