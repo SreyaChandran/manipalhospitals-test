5e4de31b68e9f5aa184bc37a1857496580b2d315
Sreya Chandran <sreya.c@appinessworld.com>
2024-06-28 17:18:55 +0530

    merge code

--- app/specialities/cardiology/page.jsx
+++ app/specialities/cardiology/page.jsx
@@ -85,8 +85,8 @@ const Cardiology = () => {
             <li>Device closure of Septal Defects such as Ventricular Septal Defects (VSD) and Atrial Septal Defects (ASD)</li>
           </ul>
         </div>
-        <div className='grid grid-cols-4 gap-4 w-full py-[60px] px-[20px] md:px-[60px] lg:px-[50px] xl:px-[100px]'>
-        {Array.from({ length: 5 }).map((_, index) => (<DoctorsCard  key=""/>))}
+        <div className='grid grid-cols-4 gap-2 w-full py-[20px] px-[20px] md:px-[60px] lg:px-[50px] xl:px-[100px]'>
+        {Array.from({ length: 8 }).map((_, index) => (<DoctorsCard  key=""/>))}
         </div>
     </div>
   )
--- components/doctorsCard.js
+++ components/doctorsCard.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import doctor_img_1 from '@/public/images/heart-specialist-in-gurgaon-col-dr-monik-mehta.png'
+import Image from 'next/image'
+import { Button } from './ui/button'
 
 const DoctorsCard = () => {
   return (
-    <div className='w-[300px] h-[400px] rounded p-0 flex flex-col gap-0 shadow-lg m-2'>
-        <div className='h-[20px] bg-gra'></div>
+    <div className='w-[290px] h-[450px] rounded-xl p-0 flex flex-col gap-0 shadow-2xl m-2'>
+        <div className='min-h-[28px] gradient-bg rounded-t-xl text-center text-white flex items-center justify-center text-sm'>Gurugram</div>
+        <Image src={doctor_img_1} 
+            alt="Manipal Hospitals" 
+            className='min-w-[100%] h-auto'
+            layout="responsive"
+            placeholder="blur"
+            blurDataURL="../public/images/heart-specialist-in-gurgaon-col-dr-monik-mehta.png"
+        />
+        <div className='h-[25px] bg-primary'></div>
+        <div className='flex flex-col items-center justify-between'>
+            <div className='border-b-[1px] border-b-slate-100 py-2'>
+                <p className='text-xs uppercase font-medium text-center'>(Col) Dr. Monik Mehta</p>
+                <p className='text-[11px] uppercase font-light text-center pt-1'>SENIOR CONSULTANT - CARDIOLOGY</p>
+            </div>
+            <div className='w-full p-4 mt-[60px]'>
+                <Button variant="default" className=' w-full'>View Profile</Button>
+            </div>
+        </div>
     </div>
   )
 }
